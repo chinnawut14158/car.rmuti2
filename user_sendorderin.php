da70f408b5302589b343e219c542c5f632c8a829
chinnawut <chinnawut14158@gmail.com>
2023-02-08 17:21:02 +0700

    fix user v.3

--- user_sendorderin.php
+++ user_sendorderin.php
@@ -33,15 +33,15 @@ if (isset($_POST['submit'])) {
 
 	mysqli_query($conn, $sql);
 
-	echo "<script>";
-	echo "alert(\"บันทึกสำเร็จ กำลังส่งข้อมูลไปยังผู้ดูแลระบบ\");";
-	echo "window.history.back()";
-	echo "</script>";
-} else {
-	echo "<script>";
-	echo "alert(\"เกิดข้อผิดพลาด บันทึกไม่สำเร็จ\");";
-	echo "window.history.back()";
-	echo "</script>";
+		echo "<script>";
+		echo "alert(\"บันทึกสำเร็จ กำลังส่งข้อมูลไปยังผู้ดูแลระบบ\");";
+		echo "window.history.back()";
+		echo "</script>";
+	} else {
+		echo "<script>";
+		echo "alert(\"เกิดข้อผิดพลาด บันทึกไม่สำเร็จ\");";
+		echo "window.history.back()";
+		echo "</script>";
 }
 if (isset($_POST['submit2'])) {
 	require_once __DIR__ . '/vendor2/autoload.php';
